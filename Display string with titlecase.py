26bdd8d58a1ae65a54f9c97679fe59dcc758548d
Pro-coder-nilima <99179769+Pro-coder-nilima@users.noreply.github.com>
2022-02-10 01:37:56 +0530

    Add files via upload

---
+++ Display string with titlecase.py
@@ -0,0 +1,8 @@
+
+# 34.write a programm to accept a string and display the string in title case.
+##(first alphabet of each word in upper)
+
+def s(sample):
+    s = "python datascience ,jupyter notebook"
+    return (s.title())
+print(s(s))
