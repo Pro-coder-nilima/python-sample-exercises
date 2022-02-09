a6572a1c08c45ad2d5dc40cb53e3376c7bcabe96
Pro-coder-nilima <99179769+Pro-coder-nilima@users.noreply.github.com>
2022-02-10 00:07:31 +0530

    Add files via upload

---
+++ count the legnth of string.py
@@ -0,0 +1,9 @@
+#36. wite a program to tyo count the length of string without using inbuilt function
+
+def each(sample):
+    count = 0
+    for i in st:
+        count = count+1
+    print(count)
+st = "programm programm"
+each(st)
