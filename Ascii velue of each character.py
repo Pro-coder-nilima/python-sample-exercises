60775e3f51e3d993d86c09c7368dcba20a312827
Pro-coder-nilima <99179769+Pro-coder-nilima@users.noreply.github.com>
2022-02-10 00:59:00 +0530

    Add files via upload

---
+++ Ascii velue of each character.py
@@ -0,0 +1,6 @@
+#31.write a program to axccept a string and display the ascii value of each character.
+
+def ds(sample):
+    for i in sample:
+        print("ASCII value of",i,"is",ord(i))
+ds(("python")) 
