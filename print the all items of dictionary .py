60e70000a14a0e9cf79680ffafa8e37b763e966e
Pro-coder-nilima <99179769+Pro-coder-nilima@users.noreply.github.com>
2022-02-10 08:24:44 +0530

    Add files via upload

---
+++ print the all items of dictionary .py
@@ -0,0 +1,16 @@
+
+
+def s(dict_):
+	
+	for i,j in dict_.items():
+		print(i,':',j)
+dict_ = {'a':100,'b':500,'c':700}
+
+s(dict_)
+
+'''
+creating empty dictinary.
+'''
+dictionary = {}
+dictionary = dict()
+print(dictionary)
