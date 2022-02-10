381f745e7ce0ab492725110358cdb0a4c8ef279d
Pro-coder-nilima <99179769+Pro-coder-nilima@users.noreply.github.com>
2022-02-10 08:15:46 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,21 @@
+# python program to perform operators
+
+def operators(x,y):
+	add = x+y;
+	print(add)
+#sub a from b	
+	sub = x-y;
+	print(sub)
+#multiply a with b
+	mul = x*y;
+	print(mul)
+#divide a by b
+	div = x/y;
+	print(div)
+#madulo of a and b
+	mod = x//y;
+	print(mod)
+#exponent of a and b
+	expo = x**y
+	print(expo)
+operators(10,5)
