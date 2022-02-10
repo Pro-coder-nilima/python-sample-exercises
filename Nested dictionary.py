f2fc40ad7de4351f8e1f1ba3685e5d29a53247e1
Pro-coder-nilima <99179769+Pro-coder-nilima@users.noreply.github.com>
2022-02-10 08:10:39 +0530

    Add files via upload

---
+++ Nested dictionary.py
@@ -0,0 +1,11 @@
+#  print the nested dictionary
+
+dict1 = {"neelima":{"surname":"misale neelima","laction":"nizamabad","id":'9'}}
+for i,j in dict1.items():
+	print(i,":",j)
+
+
+'''
+d = {"neelima":{"surname":"misale neelima","laction":"nizamabad","id":'9'}}
+print(d["neelima"]["surname"]
+'''
