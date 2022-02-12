c4721265060db33431f1e1c084fd17a3242c0216
Pro-coder-nilima <99179769+Pro-coder-nilima@users.noreply.github.com>
2022-02-12 07:11:25 +0530

    Add files via upload

---
+++ polymorphsim.py
@@ -0,0 +1,13 @@
+
+class student:
+	def show(self):                  
+		print("python")
+	def A():
+		print("DataScience")
+class employee(student):
+	def show(self):
+		print("django")
+	def A(self):
+		print("DataVisualization")
+s = student()
+s.show()
