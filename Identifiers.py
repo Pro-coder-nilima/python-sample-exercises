64f9642a9aa9a24f39e073508dcdcede5e948a4a
Nilima Misala <99179769+Pro-coder-nilima@users.noreply.github.com>
2022-02-20 20:02:20 +0530

    Add files via upload

---
+++ Identifiers.py
@@ -0,0 +1,30 @@
+
+string = 'python'
+r = string.capitalize()
+print(r)
+
+
+string = 'python'
+r = string.isalnum()
+print(r)
+
+string = 'python@123'
+r = string.isalnum()
+print(r)
+
+string = '305.3'
+r = string.isdigit()
+print(r)
+
+string = '709'
+r = string.isnumeric()
+print(r)
+
+string = 'python'
+r = string.islower() 
+print(r)
+
+string = 'Python'
+r = string.isupper()
+print(r)
+
